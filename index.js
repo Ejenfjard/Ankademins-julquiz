2e142a3d8a7ead1623a00e8c42c326f07736fd2b
Ejenjard <madeleine.ejenfjard@gmail.com>
2023-12-09 14:51:46 +0100

    added functions startQuiz, showQuestion, selectOption.

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ let questionArray = [
     },
 
     {
-        quesztion: "Pomeranskal är vanligt förekommande i en traditionell glögg?",
+        question: "Pomeranskal är vanligt förekommande i en traditionell glögg?",
         options: [
             "Sant",
             "Falskt"
@@ -98,27 +98,99 @@ let questionArray = [
 
 ];
 
-let questionDiv = document.getElementById("question");
-let optionsDiv = document.getElementById("options");
-let nextQuestionBtn = document.getElementById("nextQuestion");
-let startQuizBtn = document.getElementById("startQuiz");
+document.addEventListener("DOMContentLoaded", function () {
 
-let currentQuestion = 0
+    let nextQuestionBtn = document.getElementById("nextQuestion");
+    let startQuizBtn = document.getElementById("startQuiz");
+    let currentQuestion = 0
+
+    startQuizBtn.addEventListener("click", startQuiz);
+    nextQuestionBtn.addEventListener("click", () => {
+        //logik för att gå vidare till nästa fråga 
+
+        //Göra till en function (nextQuestionSetUp)
 
 
-function startQuiz() {
-    startQuizBtn.addEventListener("click", (event) => {
+        currentQuestion++;
+
+        showQuestion();
+    });
+
+    function startQuiz() {
+        // Tar bort startPage och ersätter det med Quizet samt kör funktionen 
+        // showQuestion
         document.getElementById("quiz-container").style.display = "block";
         document.getElementById("startPage").style.display = "none";
-
-    })
-
-
+        showQuestion();
+    }
 
 
-}
+    function showQuestion() {
+        let questionDiv = document.getElementById("question");
+        let optionsDiv = document.getElementById("options");
+        //Deklarera och definera feedback samt nollställ efter varje fråga 
+        let feedbackString = document.getElementById("feedback");
+        feedbackString.innerHTML = "";
 
-startQuiz();
+        // Visa aktuell fråga från arrayen 
+        questionDiv.innerHTML = questionArray[currentQuestion].question;
+
+        // Ta bort tidigare alternativ
+        optionsDiv.innerHTML = "";
+
+        //Skapar ett element där svarsalterativen visas
+        let btnDiv = document.createElement("div");
+        btnDiv.id = "btnOptionContainer";
+
+        questionArray[currentQuestion].options.forEach((option, index) => {
+            let btnOption = document.createElement("button");
+            btnOption.textContent = option;
+            btnOption.classList.add("option");
+            btnOption.addEventListener("click", () => selectOption(index));
+            btnDiv.appendChild(btnOption);
+        });
+
+        optionsDiv.appendChild(btnDiv);
+
+    }
+
+    function selectOption(selectedIndex) {
+        let correctAnswerIndex = questionArray[currentQuestion].correctAnswer;
+        let feedbackString = document.getElementById("feedback");
+
+        if (selectedIndex === correctAnswerIndex) {
+
+            feedbackString.innerHTML = "Rätt svar!";
+        } else {
+            feedbackString.innerHTML = "Fel svar!";
+        }
+    }
+
+});
+
+
+
+// (nextQuestionSetUp)
+
+
+// logik för att visa resultat om man svarat på sista frågan (ShowResult)
+
+
+
+
+
+
+
+
+// Lägga till en function (showResult) som visar resultatet och räkna poängen
+
+
+
+
+
+
+
+
 
 
 
